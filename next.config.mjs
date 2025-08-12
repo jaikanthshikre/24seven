59788a160d41abc3ecdc76ae307ea3fd0f732422
jaikanthshikre <jaikanthshikre36@gmail.com>
2025-08-12 17:32:52 +0530

    final push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }, // needed for static export if you use next/image
+  // optional:
+  // trailingSlash: true,
+};
 export default nextConfig;
